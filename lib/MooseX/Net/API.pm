b809c5b518e9024fa5eebe914b8d918222ce1f12
franck cuny <franck@lumberjaph.net>
2010-01-04 17:59:12 +0100

    Checking in changes prior to tagging of version 0.08.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 8335104..9478f57 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension MooseX::Net::API
    
    +0.08    Mon 04 Jan 2010 05:58:56 PM CET
    +        - api_base_url is not read-only
    +
     0.07    Mon 04 Jan 2010 11:50:22 AM CET
             - fix error reporting

--- lib/MooseX/Net/API.pm
+++ lib/MooseX/Net/API.pm
@@ -12,7 +12,7 @@ use MooseX::Net::API::Meta::Method;
 use MooseX::Net::API::Role::Serialize;
 use MooseX::Net::API::Role::Deserialize;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 my $list_content_type = {
     'json' => 'application/json',
