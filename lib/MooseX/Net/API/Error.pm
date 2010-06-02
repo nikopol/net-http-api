14a0f6b2b3ae8a30dbf5ee0c069acc37a3472786
franck cuny <franck@lumberjaph.net>
2010-06-02 11:34:57 +0200

    use JSON, add POD

--- lib/MooseX/Net/API/Error.pm
+++ lib/MooseX/Net/API/Error.pm
@@ -1,12 +1,12 @@
 package MooseX::Net::API::Error;
 
 use Moose;
-use JSON::XS;
+use JSON;
 use Moose::Util::TypeConstraints;
 use overload '""' => \&error;
 
 subtype error => as 'Str';
-coerce error => from 'HashRef' => via { encode_json $_};
+coerce error => from 'HashRef' => via { JSON::encode_json $_};
 
 has http_error => (
     is      => 'ro',
@@ -31,3 +31,34 @@ sub error {
 1;
 
 __END__
+
+=head1 NAME
+
+MooseX::Net::API::Error
+
+=head1 SYNOPSIS
+
+    MooseX::Net::API::Error->new(reason => "'useragent' is required");
+
+or
+
+    MooseX::Net::API::Error->new()
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck@lumberjaph.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright 2009, 2010 by Linkfluence
+
+http://linkfluence.net
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
